4fdea34ac47f9740e0261eaf66dad63b9851776f
Eli Citron <ebcitron@gmail.com>
2020-04-29 16:38:30 -0500

    Sanity Commit

---
+++ portfolio/src/resumeData.js
@@ -0,0 +1,10 @@
+let resumeData = {
+    "imagebaseurl":"http://localhost:3000/",
+    "name":"Eli Benjamin Citron",
+    "role":"Full Stack Web Developer",
+    "phoneNumber":"1(224)307-0844",
+    "address":"928 Judson Ave, Evanston Illinois",
+    
+}
+
+export default resumeData;
