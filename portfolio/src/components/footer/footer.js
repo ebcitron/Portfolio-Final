b61a4fb579cb45b260aa6fea1047eb28ce079546
Eli Citron <ebcitron@gmail.com>
2020-05-01 00:47:47 -0500

    Finishing up

--- portfolio/src/components/footer/footer.js
+++ portfolio/src/components/footer/footer.js
@@ -7,7 +7,7 @@ class Footer extends Component{
             <footer>
         <div className="row">
           <div className="twelve columns">
-            <ul className="social-links">
+            {/* <ul className="social-links">
               <li><a href="#"><i className="fa fa-facebook" /></a></li>
               <li><a href="#"><i className="fa fa-twitter" /></a></li>
               <li><a href="#"><i className="fa fa-google-plus" /></a></li>
@@ -15,11 +15,8 @@ class Footer extends Component{
               <li><a href="#"><i className="fa fa-instagram" /></a></li>
               <li><a href="#"><i className="fa fa-dribbble" /></a></li>
               <li><a href="#"><i className="fa fa-skype" /></a></li>
-            </ul>
-            <ul className="copyright">
-              <li>© Copyright 2014 CeeVee</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>   
-            </ul>
+            </ul> */}
+            
           </div>
           <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open" /></a></div>
         </div>
